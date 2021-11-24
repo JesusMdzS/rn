ed263849ef5249bd21295723bc835b48e535ee4e
Jesus Mendoza <jmendoza@quaxar.com>
2021-11-24 17:44:30 -0600

    remove ts

---
+++ src/hooks/useUsuarios.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useUsuarios = () => {
+  const [usuarios, setusuarios] = useState([]);
+  const url = "https://reqres.in/api/users?per_page=12";
+
+  const loadUsuarios = async () => {
+    await axios
+      .get(url)
+      .then((response) => {
+        setusuarios(response.data.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  useEffect(() => {
+    loadUsuarios();
+  }, []);
+
+  // console.log(usuarios);
+  return {
+    usuarios,
+  };
+};
---
+++ src/navigation/Navigation.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginScreen } from "../screens/LoginScreen";
+import { MenuScreen } from "../screens/MenuScreen";
+import { ListUsersScreen } from "../screens/ListUsersScreen";
+
+const Stack = createStackNavigator();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{
+          gestureEnabled: true,
+          title: "Login",
+        }}
+      />
+      <Stack.Screen
+        name="MenuScreen"
+        component={MenuScreen}
+        options={{
+          gestureEnabled: false,
+          title: "Quaxar",
+        }}
+      />
+      <Stack.Screen
+        name="ListUsersScreen"
+        component={ListUsersScreen}
+        options={{
+          gestureEnabled: false,
+          title: "Usuarios",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
